23adae5e7b33e322562548f0efd65c667da9ffa2
Stacey Astoveza <staceyastoveza@transfix.io>
2017-09-13 13:18:45 -0400

    added restaurant table model and controller

--- spunch/db/schema.rb
+++ spunch/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170912172430) do
+ActiveRecord::Schema.define(version: 20170913171348) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,17 @@ ActiveRecord::Schema.define(version: 20170912172430) do
     t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "table_restaurants", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.string "image"
+    t.integer "rating"
+    t.float "coordinates"
+    t.integer "zipcode"
+    t.string "url"
+    t.string "phone"
   end
 
 end
